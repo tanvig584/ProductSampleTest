7e9c29f2c537f39601781e518f07e619e4fedba4
tanvig584 <46836079+tanvig584@users.noreply.github.com>
2019-01-19 12:08:19 +0530

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsApp.Models;
+using ProductsApp.ViewModel;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult ProductList(string searchString)
+        {
+            if (String.IsNullOrEmpty(searchString))
+            {
+                var viewModel =
+                          from pd in _context.Product
+                          join psc in _context.ProductSubCategory on pd.subCategoryId equals psc.subCategoryId
+                          join pc in _context.ProductCategory on psc.categoryId equals pc.categoryId
+                          select new ProductsListViewModel { products = pd, category = pc, subcategory = psc };
+
+                return View(viewModel);
+            }
+            else
+            {
+                var viewModel =
+                          from pd in _context.Product
+                          join psc in _context.ProductSubCategory on pd.subCategoryId equals psc.subCategoryId
+                          join pc in _context.ProductCategory on psc.categoryId equals pc.categoryId
+                          where pc.categoryName == searchString
+                          select new ProductsListViewModel { products = pd, category = pc, subcategory = psc };
+                return View(viewModel);
+
+            }
+        }
+ 
+        public ActionResult CreateProduct()
+        {
+           
+             return View();
+        }
+        [HttpPost]
+        public ActionResult SaveProduct(Product product)
+        {
+            _context.Product.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("ProductList","Products");
+        }
+    }
+}
